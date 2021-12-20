1d7e9f5ede3da114729736acd33496f8218396c9
Jason Goldberger <jasongoldberger@gmail.com>
2021-12-20 12:22:03 -0700

    fix: split space-and-unicode into space-then-unicode during iteration
    Signed-off-by: Jason Goldberger <jasongoldberger@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -50,8 +50,30 @@ impl<'a> AsRef<str> for Token<'a> {
 
 
 /// Iterates the Tokens of a given text.
+
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+enum GraphemeKind {
+    Whitespace,
+    LineTerminator,
+    Char
+}
+
+impl GraphemeKind {
+    fn from_str(s: &str) -> GraphemeKind {
+        use GraphemeKind as Gk;
+        match s {
+            " " => Gk::Whitespace,
+            "\n" => Gk::LineTerminator,
+            "\r\n" => Gk::LineTerminator,
+            _ => Gk::Char,
+        }
+    }
+}
+
 pub struct TokenIter<'a> {
     it: UWordBounds<'a>,
+    state: &'a str,
     pos: Position,
 }
 
@@ -60,25 +82,66 @@ impl<'a> TokenIter<'a> {
     pub fn new(text: &'a str) -> TokenIter<'a> {
         TokenIter {
             it: text.split_word_bounds(),
+            state: "",
             pos: Position::new(),
         }
     }
+}
 
+
+fn split_text<'a>(txt: &'a str) -> (&'a str, &'a str) {
+    let mut offset = 0;
+    let mut state = None;
+    let mut g_iter = txt.graphemes(true);
+    while let Some(g) = g_iter.next() {
+        // println!("GRAPHEME: {:?}", g);
+        match state {
+            None => {
+                state = Some(GraphemeKind::from_str(&g[..]));
+                // println!("None STATE: {:?} {:?}", g, state);
+                offset += g.len();
+                continue;
+            },
+            Some(state_kind) => {
+                let g_kind = GraphemeKind::from_str(&g[..]);
+                if g_kind == state_kind {
+                    // println!("Same STATE: {:?} {:?} {:?}", g, state_kind, g_kind);
+                    offset += g.len();
+                    continue;
+                }
+                // println!("Diff STATE: {:?} {:?} {:?}", g, state_kind, g_kind);
+                break;
+            }
+        }
+    }
+    let left = &txt[0..offset];
+    let right = &txt[offset..];
+    // println!("SPLIT TEXT: {:?} {:?}", left, right);
+    (left, right)
 }
 
 impl<'a> Iterator for TokenIter<'a> {
     type Item = Token<'a>;
 
     fn next(&mut self) -> Option<Token<'a>> {
-        match self.it.next() {
-            Some(txt) => {
-                let pos = self.pos.clone();
-                self.pos = pos.update(&txt[..]);
-                Some(Token::new(txt, pos))
-            },
-            None => None
-        }   
+        if self.state.len() > 0 {
+            let pos = self.pos.clone();
+            let (txt, rest) = split_text(self.state);
+            self.state = rest;
+            self.pos = pos.update(&txt[..]);
+            return Some(Token::new(txt, pos))
+        }
+        if let Some(txt) = self.it.next() {
+            let pos = self.pos.clone();
+            let (head, rest) = split_text(txt);
+            self.pos = pos.update(&head[..]);
+            self.state = rest;
+            return Some(Token::new(head, pos));
+        }
+        return None   
     }
+
+    
 }
 
 
@@ -93,7 +156,11 @@ pub struct Position {
 impl Position {
     /// Returns a Position as line 1, column 1, and byte_offset 0.
     fn new() -> Position {
-        Position { line: 1, column: 1, byte_offset: 0 }
+        Position::at(1, 1, 0)
+    }
+
+    fn at(line: usize, column: usize, byte_offset: usize) -> Position {
+        Position { line, column, byte_offset }
     }
 
     /// Given a position and some text `txt` returns a new, updated position.
@@ -129,7 +196,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn iterates_words_punctuation_spaces_and_newlines_correctly() {
+    fn works_for_ascii_correctly() {
         let text = "hello: (world)\n\n \r\n  \"thing\"";
         let mut it = TokenIter::new(text);
         assert_eq!(it.next().unwrap(), Token::new("hello", Position{line: 1, column: 1, byte_offset: 0}));
@@ -148,4 +215,37 @@ mod tests {
         assert_eq!(it.next().unwrap(), Token::new("\"", Position{line: 4, column: 9, byte_offset: 27}));
         assert_eq!(it.next(), None);
     }
+
+    #[test]
+    fn works_when_unicode_byte_order_mark_is_first() {
+        let text = "\u{FEFF} okay";
+        let mut it = TokenIter::new(text);
+        assert_eq!(it.next().unwrap(), Token::new("\u{FEFF}", Position{line: 1, column: 1, byte_offset: 0}));
+        assert_eq!(it.next().unwrap(), Token::new(" ", Position{line: 1, column: 2, byte_offset: 3}));
+        assert_eq!(it.next().unwrap(), Token::new("okay", Position{line: 1, column: 3, byte_offset: 4}));
+    }
+
+    #[test]
+    fn works_when_unicode_byte_order_mark_is_not_first() {
+        let text = "one \u{FEFF} two";
+        let mut it = TokenIter::new(text);
+        assert_eq!(it.next().unwrap(), Token::new("one", Position{line: 1, column: 1, byte_offset: 0}));
+        assert_eq!(it.next().unwrap(), Token::new(" ", Position{line: 1, column: 4, byte_offset: 3}));
+        assert_eq!(it.next().unwrap(), Token::new("\u{FEFF}", Position{line: 1, column: 5, byte_offset: 4}));
+        assert_eq!(it.next().unwrap(), Token::new(" ", Position{line: 1, column: 6, byte_offset: 7}));
+        assert_eq!(it.next().unwrap(), Token::new("two", Position{line: 1, column: 7, byte_offset: 8}));
+    }
+
+    #[test]
+    fn keeps_words_intact() {
+        let text = "\u{FEFF}José Valim";
+        let mut it = TokenIter::new(text);
+        assert_eq!(it.next().unwrap(), Token::new("\u{FEFF}", Position{line: 1, column: 1, byte_offset: 0}));
+        assert_eq!(it.next().unwrap(), Token::new("José", Position{line: 1, column: 2, byte_offset: 3}));
+        assert_eq!(it.next().unwrap(), Token::new(" ", Position{line: 1, column: 6, byte_offset: 8}));
+        assert_eq!(it.next().unwrap(), Token::new("Valim", Position{line: 1, column: 7, byte_offset: 9}));
+    }
 }
+
+
+
